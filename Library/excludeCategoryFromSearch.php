af5e6e98a70d78d303557fda1e8ec1e2f1cffe78
Mahesh Ruparel <maheshruparel@gmail.com>
2021-01-08 11:55:08 +0530

    Update excludeCategoryFromSearch.php

--- Library/excludeCategoryFromSearch.php
+++ Library/excludeCategoryFromSearch.php
@@ -1,1 +1,17 @@
+function exclude_category_from_search($query) {
+    if ($query->is_search) {
+		if(isset($_COOKIE['yourEdition']) || $_REQUEST["ViewEdition"]){
+            if($_COOKIE['yourEdition']=="US" || $_REQUEST["ViewEdition"]=="US"){
+                // $query->set('cat', '-158');
+                // $query->set('cat', '+157');
+            }
+            elseif($_COOKIE['yourEdition']=="Global" || $_REQUEST["ViewEdition"]=="Global"){
+                // $query->set('cat', '-157');
+                // $query->set('cat', '+158');
+            }
+        }
+    }
+    return $query;
+}
+add_filter('pre_get_posts','exclude_category_from_search', 1);
 
