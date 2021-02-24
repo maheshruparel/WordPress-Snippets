990e749f5c295fadff8180eff7fdfb4cabe65008
Mahesh Ruparel <maheshruparel@gmail.com>
2021-02-24 14:37:17 +0530

    Update excludePagesFromSeach.php

--- Library/excludePagesFromSeach.php
+++ Library/excludePagesFromSeach.php
@@ -1,17 +1,15 @@
 <?php
+
 // add in functions.php
+
 function exclude_pages_from_search($query) {
     if ($query->is_search) {
-		if(isset($_COOKIE['yourEdition']) || $_REQUEST["ViewEdition"]){
+	if(isset($_COOKIE['yourEdition']) || $_REQUEST["ViewEdition"]){
             if($_COOKIE['yourEdition']=="US" || $_REQUEST["ViewEdition"]=="US"){
                 $query->set('post__not_in', array_merge(array(), ['8236', '8237']));
-                // $query->set('cat', '-158');
-                // $query->set('cat', '+157');
             }
             elseif($_COOKIE['yourEdition']=="Global" || $_REQUEST["ViewEdition"]=="Global"){
                 $query->set('post__not_in', array_merge(array(), ['8237']));
-                // $query->set('cat', '-157');
-                // $query->set('cat', '+158');
             }
         }
     }
