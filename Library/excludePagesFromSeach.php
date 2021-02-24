335e5aa70900cdb9b0d7a3510371ebb9a0bc79c4
Mahesh Ruparel <maheshruparel@gmail.com>
2021-02-24 14:36:02 +0530

    Create excludePagesFromSeach.php

---
+++ Library/excludePagesFromSeach.php
@@ -0,0 +1,20 @@
+<?php
+// add in functions.php
+function exclude_pages_from_search($query) {
+    if ($query->is_search) {
+		if(isset($_COOKIE['yourEdition']) || $_REQUEST["ViewEdition"]){
+            if($_COOKIE['yourEdition']=="US" || $_REQUEST["ViewEdition"]=="US"){
+                $query->set('post__not_in', array_merge(array(), ['8236', '8237']));
+                // $query->set('cat', '-158');
+                // $query->set('cat', '+157');
+            }
+            elseif($_COOKIE['yourEdition']=="Global" || $_REQUEST["ViewEdition"]=="Global"){
+                $query->set('post__not_in', array_merge(array(), ['8237']));
+                // $query->set('cat', '-157');
+                // $query->set('cat', '+158');
+            }
+        }
+    }
+    return $query;
+}
+add_filter('pre_get_posts','exclude_pages_from_search', 1);
